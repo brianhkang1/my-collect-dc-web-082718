aedd0df2c9d5faa5169baa86bfbc2eda8fc050e0
Brian Kang <brianhkang1@gmail.com>
2018-08-14 19:36:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
 def my_collect(array)
   if block_given?
     i = 0
- 
+    
+    new_array = []
     while i < array.length
       yield(array[i])
+      new_array.push(array[i])
       i += 1
     end
     
